cf28d2bcadcdeb43e5b9880ff3cc471ccd9b33cb
yourname <youremail>
2019-04-10 22:06:30 +0800

    banner

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+// require styles
+import 'swiper/dist/css/swiper.css'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick' // 引入fastClick库以解决某些手机click事件延迟3s钟问题
@@ -9,7 +12,7 @@ import 'styles/border.css' // 引入border.css以解决不同手机1像素边框
 import 'styles/iconfont.css' // 引入iconfont
 Vue.config.productionTip = false
 fastClick.attach(document.body) // 使用fastClick
-
+Vue.use(VueAwesomeSwiper)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+  <swiper :options="swiperOption">
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl" />
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: 'http://img5.imgtn.bdimg.com/it/u=3409737275,2744185252&fm=26&gp=0.jpg'
+      }, {
+        id: '002',
+        imgUrl: 'http://img0.imgtn.bdimg.com/it/u=2033318980,3797978556&fm=26&gp=0.jpg'
+      }, {
+        id: '003',
+        imgUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=330142140,1934812749&fm=173&app=25&f=JPEG?w=640&h=373&s=FF24556EEC2EA73449E039B902003013'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #633eff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
